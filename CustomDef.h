8e4adb6e436feba2abf439b18418bb7be466ddba
chevalir <chevalir01@gmail.com>
2017-01-29 10:01:30 +0100

    Add files via upload

---
+++ CustomDef.h
@@ -0,0 +1,20 @@
+// @@RC Define
+#define RC_VERSION
+#define INDEX_CUSTOM_RF_PROBES 0  // first index in the custom array for the RADIO PROBES
+#define INDEX_CUSTOM_CONNECTED_PROBES 8 // first index in the custom array for the CONNECTED PROBES
+#define RF_PROBE_CHACON_ID 9999 // use to identified message sent by "RF Probe" (must be changed in case of conflic with Chacon device)
+#define RF_PROBE_CHACON_ID_MASK 9999000 // mask used to retreive only data
+#define RF_PROBE_CHACON_OFFSET 9998999 // mask used to retreive only data
+
+#define GROUP_MASQ 1000 // chacon group flag in message addresse possible value 1000 if set, 0 if not set
+// define CUSTOM_APPLIQUE_SALON 6 // custom use for appliques salon
+// define SPOT_BUREAU_CUSTOM_COMMAND 6
+// define SPOT_BUREAU_COMMAND_PIN 6
+// define SPOT_BUREAU_STATE_PIN 5
+// define CUSTOM_RETOUT_BRULEUR 7
+#define CUSTOM_PROBE_OFFSET 0   // new configuration offet move from 10 to 0
+#define CUSTOM_STATUS_OFFSET 20
+
+
+// @@RC                 
+
